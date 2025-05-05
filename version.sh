00255e10940cd780ec02092391ec23fd714d82d8
Samuel Urso <surso@altimetrik.com>
2025-05-05 09:49:33 -0500

    Testing version and hash

---
+++ version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash   
+
+VERSION=local-$(date +%Y%m%d)
+COMMIT_HASH=$(git rev-parse --short HEAD)
+go build -ldflags "-X main.version=$VERSION -X main.commitHash=$COMMIT_HASH" -o hello-world
