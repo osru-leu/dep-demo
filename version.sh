cc0d342f93fc4ce66af8ed2dc241a861f049a16c
Samuel Urso <surso@altimetrik.com>
2025-05-05 10:18:59 -0500

    Testing version.sh script

--- version.sh
+++ version.sh
@@ -2,6 +2,5 @@
 
 VERSION=local-$(date +%Y%m%d)
 COMMIT_HASH=$(git rev-parse --short HEAD)
-RUN_NUMBER=${1:-local}
 
-go build -ldflags "-X main.version=$VERSION.$RUN_NUMBER -X main.commitHash=$COMMIT_HASH" -o hello-world
+go build -ldflags "-X main.version=$VERSION -X main.commitHash=$COMMIT_HASH" -o hello-world
