590656da0c03b6bc75c871226b4664e3c80b9265
Samuel Urso <surso@altimetrik.com>
2025-05-06 13:35:59 -0500

    Added no server run if in CI

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
-
 package main
 
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 var (
@@ -15,14 +15,19 @@ func main() {
 	// Print version and commit hash to the console
 	fmt.Printf("Hello, World!\nVersion: %s\nCommit Hash: %s\n", version, commitHash)
 
-	// Define a simple HTTP handler
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, k8 using scratch\nVersion: %s\nCommit Hash: %s\n", version, commitHash)
-	})
+	// Check if running in a CI environment
+	if os.Getenv("CI") != "true" {
+		// Define a simple HTTP handler
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, k8 using scratch\nVersion: %s\nCommit Hash: %s\n", version, commitHash)
+		})
 
-	// Start the HTTP server
-	fmt.Println("Server is running at http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Printf("Error starting server: %s\n", err)
+		// Start the HTTP server
+		fmt.Println("Server is running at http://localhost:8080")
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			fmt.Printf("Error starting server: %s\n", err)
+		}
+	} else {
+		fmt.Println("Running in CI environment, server not started.")
 	}
 }
