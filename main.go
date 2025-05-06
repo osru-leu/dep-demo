12711ea83cffe8f69927d2dd2e55decacd81b7f2
Samuel Urso <surso@altimetrik.com>
2025-05-06 13:30:12 -0500

    Added server back to main.go

--- main.go
+++ main.go
@@ -1,21 +1,28 @@
+
 package main
 
 import (
 	"fmt"
-	// "net/http"
+	"net/http"
 )
 
 var (
-	version string
+	version    string
 	commitHash string
 )
 
 func main() {
+	// Print version and commit hash to the console
 	fmt.Printf("Hello, World!\nVersion: %s\nCommit Hash: %s\n", version, commitHash)
+
+	// Define a simple HTTP handler
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, k8 using scratch\nVersion: %s\nCommit Hash: %s\n", version, commitHash)
+	})
+
+	// Start the HTTP server
+	fmt.Println("Server is running at http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
 }
-// func main() {
-// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-// 		fmt.Fprintf(w, "Hello, k8 using scratch")
-// 	})
-// 	http.ListenAndServe(":8080", nil)
-// }
